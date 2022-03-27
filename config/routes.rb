215ec803634663706585bd9dfe53c4a6680e9540
Hampus Davidsson <hampus.davidsson@gmail.com>
2022-03-27 19:56:08 +0200

    stripe checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  post "checkout/create", to: "checkout#create"
 
   root "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
