fc46e791795612083853fc7bbca20d6406b50a8f
Hampus Davidsson <hampus.davidsson@gmail.com>
2022-03-27 20:01:18 +0200

    added sales count to product!Q

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_27_163950) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_27_175815) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sales_count", default: 0, null: false
   end
 
 end
