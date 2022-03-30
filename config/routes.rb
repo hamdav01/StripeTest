be2c4ed0121b791aa3d396e92a8adb47fa044c38
Hampus Davidsson <hampus.davidsson@gmail.com>
2022-03-30 20:10:43 +0200

    t

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post "checkout/create", to: "checkout#create"
   resources :webhooks, only: [:create]
   root "products#index"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
